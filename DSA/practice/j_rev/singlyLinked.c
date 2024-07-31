b35c36789829efe9ef586860783fc4cc0cf6450f
sovon1 <sovon2018@gmail.com>
2024-07-31 18:36:16 +0600

    ,

--- DSA/practice/j_rev/singlyLinked.c
+++ DSA/practice/j_rev/singlyLinked.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<malloc.h>
+#define n printf("\n"); 
 struct node {
   int data;
   struct node* next;
@@ -47,13 +48,52 @@ void insertionAtTheBeg(struct node **head)
     *head=newNode;
 
 }
+
+int len(struct node *head)
+{
+    int count=0;
+    struct node * temp = head;
+    while (temp!=NULL)  
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count;
+    
+}
+void insertionAfterAspecific(struct node **head,int location)
+{
+    struct node *temp = *head;
+    for(int i=0;i<location-1;i++)
+    {
+      temp= temp->next;
+    }
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter the data you want to inset : ");
+    scanf("%d",&newNode->data);
+    newNode->next = temp->next;
+    temp->next=newNode;
+}
 int main()
 {
     struct node *head = NULL;
     createLinkedList(&head);
     display(head); 
 //let's perform insertion 
-printf("\n");
+n
    insertionAtTheBeg(&head);
    display(head);
+   //finding out the length
+   int length = len(head);
+   printf("The length is %d",length);
+//insertion at a specific position
+n
+int location;
+printf("Enter the location you want to insert : ");
+scanf("%d",&location);
+insertionAfterAspecific(&head,location);
+display(head);
+
+
+
 }
